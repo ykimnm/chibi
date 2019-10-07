afdd168bcd47a889817ad557275301c76319e348
YOKOI Minami <21716106@jwu.ac.jp>
2019-10-07 16:32:35 +0900

    commit1

--- rational.py
+++ rational.py
@@ -6,6 +6,15 @@ class Q(object):
             self.a=a
              self.b = b
   def __repr__(self):
+      if self.b == 1 :
+          return str(self.a)
+          return f`{self.a}/{self.b}`
+          def add(self,q):
+              a = self.a
+              b = self.b
+              c = q . a
+              d = q . b
+              return Q(. . .,...)        
       return f'{self.a}/{self.b}'
 q = Q(1,2)
 print (q)
