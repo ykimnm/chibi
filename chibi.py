a2c529da2b2ecfb2a54d28064a12f2b4775a72bc
YOKOI Minami <21716106@jwu.ac.jp>
2019-12-16 16:23:54 +0900

    chibi

--- chibi.py
+++ chibi.py
@@ -32,3 +32,80 @@ class Binary(Expr):
     def __repr__(self):
         classname = self.__class__.__name__
         return f'{classname}({self.left},{self.right})'
+    class Add(Binary):
+        __slots__ = ['left', 'right']
+        def eval(self, env: dict):
+            return self.left.eval(env) + self.right.eval(env)
+    class Sub(Binary):
+        __slots__ = ['left', 'right']
+        def eval(self, env: dict):
+            return self.left.eval(env) - self.right.eval(env)
+    class Mul(Binary):
+        __slots__ = ['left', 'right']
+        def eval(self, env: dict):
+            return self.left.eval(env) * self.right.eval(env)
+    class Div(Binary):
+        __slots__ = ['left', 'right']
+        def eval(self, env: dict):
+            return self.left.eval(env) // self.right.eval(env)
+    class Mod(Binary):
+        __slots__ = ['left', 'right']
+        def eval(self, env: dict):
+            return self.left.eval(env) % self.right.eval(env)
+    class Var(Expr):
+        __slots__ = ['name']
+        def __init__(self, name):
+            self.name = name
+        def eval(self, env: dict):
+            if self.name in env:
+                return env[self.name]
+            raise NameError(self.name)
+    class Assign(Expr):
+        __slots__ = ['name', 'e']
+        def __init__(self, name, e):
+            self.name = name
+            self.e = Expr.new(e)
+        def eval(self, env):
+            env[self.name] = self.e.eval(env)
+            return env[self.name]
+    def conv(tree):
+        if tree == 'Block':
+            return conv(tree[0])
+        if tree == 'Val' or tree == 'Int':
+            return Val(int(str(tree)))
+        if tree == 'Add':
+            return Add(conv(tree[0]), conv(tree[1]))
+        if tree == 'Sub':
+            return Sub(conv(tree[0]), conv(tree[1]))    
+        if tree == 'Mul':
+            return Mul(conv(tree[0]), conv(tree[1]))
+        if tree == 'Div':
+            return Div(conv(tree[0]), conv(tree[1]))
+        if tree == 'Mod':
+            return Mod(conv(tree[0]), conv(tree[1]))
+        if tree == 'Var':
+            return Var(str(tree))
+        if tree == 'LetDecl':
+            return Assign(str(tree[0]), conv(tree[1]))
+        print('@TODO', tree.tag, repr(tree))
+        return Val(str(tree))
+    def run(src: str, env: dict):
+        tree = parser(src)
+        if tree.isError():
+            print(repr(tree))
+        else:
+            e = conv(tree)
+            print('env', env)
+            print(e.eval(env))
+    def main():
+        try:
+            env = {}
+            while True:
+                s = input('>>> ')
+                if s == '':
+                    break
+                run(s, env)
+        except EOFError:
+            return
+    if __name__ == '__main__':
+        main()
