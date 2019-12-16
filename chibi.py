855961237eeb8e15ff91d232924943493df9a021
YOKOI Minami <21716106@jwu.ac.jp>
2019-12-16 16:05:41 +0900

    chibi

--- chibi.py
+++ chibi.py
@@ -14,3 +14,21 @@ class Expr(object):
         if isinstance(v, Expr):
             return v
         return Val(v)
+class Val(Expr):
+    __slots__ = ['value']
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return f'Val({self.value})'
+    def eval(self, env: dict):
+        return self.value
+e = Val(0)
+assert e.eval({}) == 0
+class Binary(Expr):
+    __slots__ = ['left', 'right']
+    def __init__(self, left, right):
+        self.left = Expr.new(left)
+        self.right = Expr.new(right)
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
